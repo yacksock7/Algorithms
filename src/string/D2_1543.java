a3dc93edb97c2db0df0aa72e8da84af7d8377939
youngrackchoi <yacksock3@naver.com>
2025-03-04 11:50:07 +0900

    Day 2 - 문자열 다루기
    1. 1919 - 애너그램 만들기
    2. 1157 - 단어 공부
    3. 1543 - 문서검색

--- src/string/D2_1543.java
+++ src/string/D2_1543.java
@@ -28,7 +28,7 @@ import java.io.*;
  */
 
 
-public class _2_1543 {
+public class D2_1543 {
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
              BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
